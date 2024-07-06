fdd9fe3f1806aa04f45bd72e144223cc13975f1f
Nishadchowdhury <Nishadhj111@gmail.com>
2024-07-06 21:04:31 +0600

    first commit with a portion of the home page

---
+++ components/custom/MaxWidthWrapper.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils";
+
+interface pageProps {
+    className?: string;
+    children: React.ReactNode;
+}
+const MaxWidthWrapper: React.FC<pageProps> = ({ className, children }) => {
+
+
+    return (
+        <div className={cn("h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20 ", className)} >
+            {children}
+        </div>
+    )
+
+}
+
+export default MaxWidthWrapper;
