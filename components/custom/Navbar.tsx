0fc902e35ae32e98413719b7d6c67dd2ebc9f59e
Nishadchowdhury <Nishadhj111@gmail.com>
2024-07-09 08:02:12 +0600

    navbar buttons added

--- components/custom/Navbar.tsx
+++ components/custom/Navbar.tsx
@@ -1,6 +1,8 @@
 
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+import { buttonVariants } from "../ui/button";
+import { ArrowRight } from "lucide-react";
 
 interface pageProps {
 
@@ -9,6 +11,7 @@ const Navbar: React.FC<pageProps> = () => {
 
 
     const user = undefined;
+    const isAdmin = undefined;
 
     return (
         <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all' >
@@ -26,11 +29,63 @@ const Navbar: React.FC<pageProps> = () => {
                         {user
                             ?
                             <>
-                                <Link href={"/api/auth/logout"} className={""} ></Link>
+                                <Link href={"/api/auth/logout"}
+                                    className={buttonVariants({ // this function gives all the structure of a shadeCn ui's button
+                                        size: "sm",
+                                        variant: "ghost",
+                                    })} >
+                                    Sign out
+                                </Link>
+
+                                {isAdmin ?
+                                    <Link href={"/api/auth/logout"}
+                                        className={buttonVariants({
+                                            size: "sm",
+                                            variant: "ghost",
+                                        })} >
+                                        Dashboard ✨
+                                    </Link> : null
+                                }
+
+                                <Link href={"/configure/upload"}
+                                    className={buttonVariants({
+                                        size: "sm",
+                                        className: "hidden sm:flex items-center gap-1"
+                                    })} >
+                                    Create case
+                                    <ArrowRight className="ml-1.5 size-5" />
+                                </Link>
+
                             </>
                             :
                             <>
+                                <Link href={"/api/auth/register"}
+                                    className={buttonVariants({ // this function gives all the structure of a shadeCn ui's button
+                                        size: "sm",
+                                        variant: "ghost",
+                                    })} >
+                                    Sign up
+                                </Link>
 
+                                <Link href={"/configure/upload"}
+                                    className={buttonVariants({
+                                        size: "sm",
+                                        variant: "ghost"
+                                    })} >
+                                    Log in
+                                    <ArrowRight className="ml-1.5 size-5" />
+                                </Link>
+
+                                <div className="h-8 w-px bg-zinc-200 hidden sm:block " />
+
+                                <Link href={"/configure/upload"}
+                                    className={buttonVariants({
+                                        size: "sm",
+                                        className: "hidden sm:flex items-center gap-1"
+                                    })} >
+                                    Create case
+                                    <ArrowRight className="ml-1.5 size-5" />
+                                </Link>
                             </>
                         }
                     </div>
