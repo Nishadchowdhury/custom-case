6f5051d463b5a7479c61f47fa1f0c6d967831554
Nishadchowdhury <Nishadhj111@gmail.com>
2025-01-05 13:06:22 +0600

    phone's models list is added

--- app/configure/design/DesignConfiscator.tsx
+++ app/configure/design/DesignConfiscator.tsx
@@ -7,8 +7,11 @@ import NextImage from 'next/image'
 import { Rnd } from 'react-rnd'
 import { RadioGroup } from '@headlessui/react'
 import { useState } from "react";
-import { COLORS } from "@/validators/option-validator";
+import { COLORS, MODELS } from "@/validators/option-validator";
 import { Label } from "@/components/ui/label";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Check, ChevronsUpDown } from "lucide-react";
 
 interface pageProps {
     configId: string;
@@ -23,9 +26,15 @@ interface pageProps {
 const DesignConfiscator: React.FC<pageProps> = ({ configId, imageUrl, imageDimensions }) => {
 
 
-    const [options, setOptions] = useState<{ color: (typeof COLORS)[number] }>({
-        color: COLORS[0]
-    })
+    const [options, setOptions] = useState<{
+        color: (typeof COLORS)[number]
+        model: (typeof MODELS.options)[number]
+    }>(
+        {
+            color: COLORS[0],
+            model: MODELS.options[0]
+        }
+    )
 
     return (
         <div className='relative mt-20 grid grid-cols-3 mb-20 pb-20 ' >
@@ -105,49 +114,87 @@ const DesignConfiscator: React.FC<pageProps> = ({ configId, imageUrl, imageDimen
                         <div
                             className="relative mt-4 h-full flex flex-col justify-between "
                         >
-                            <RadioGroup
-                                value={options.color}
-                                onChange={(value) => {
-                                    setOptions((prev) => {
-                                        return {
-                                            ...prev,
-                                            color: value
-                                        }
-                                    })
-                                }}
-                            >
-                                <Label >
-                                    Color: {options.color.label}
-                                </Label>
+                            <div className="flex flex-col gap-6 " >
 
-                                <div
-                                    className="mt-3 flex items-center space-x-3"
+                                <RadioGroup
+                                    value={options.color}
+                                    onChange={(value) => {
+                                        setOptions((prev) => {
+                                            return {
+                                                ...prev,
+                                                color: value
+                                            }
+                                        })
+                                    }}
                                 >
-                                    {
-                                        COLORS.map((color) => (
-                                            <RadioGroup.Option
-                                                key={color.label}
-                                                value={color}
-                                                className={({ active, checked }) => cn('relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent ',
-                                                    {
-                                                        [`border-${color.tw}`]: active || checked // cn function doesn't support template string directly in conditions so [``] needed here.
-                                                    }
-                                                )
-                                                }
-                                            >
-                                                <span
-                                                    className={cn(`bg-${color.tw}`, "h-8 w-8 rounded-full border-black border-opacity-10  ")}
-                                                />
-                                            </RadioGroup.Option>
-                                        ))
-                                    }
-                                </div>
-                            </RadioGroup>
-                        </div>
+                                    <Label >
+                                        Color: {options.color.label}
+                                    </Label>
 
+                                    <div
+                                        className="mt-3 flex items-center space-x-3"
+                                    >
+                                        {
+                                            COLORS.map((color) => (
+                                                <RadioGroup.Option
+                                                    key={color.label}
+                                                    value={color}
+                                                    className={({ active, checked }) => cn('relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent ',
+                                                        {
+                                                            [`border-${color.tw}`]: active || checked // cn function doesn't support template string directly in conditions so [``] needed here.
+                                                        }
+                                                    )
+                                                    }
+                                                >
+                                                    <span
+                                                        className={cn(`bg-${color.tw}`, "h-8 w-8 rounded-full border-black border-opacity-10  ")}
+                                                    />
+                                                </RadioGroup.Option>
+                                            ))
+                                        }
+                                    </div>
+                                </RadioGroup>
+                                <div className="relative flex flex-col gap-3 w-full ">
+                                    <Label>Model</Label>
+                                    <DropdownMenu >
+                                        <DropdownMenuTrigger asChild /*the child will be printed here*/ >
+                                            <Button
+                                                variant={'outline'}
+                                                role="combobox"
+                                                className="w-full justify-between"
+                                            >
+                                                {options.model.label}
+                                                <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50 " />
+                                            </Button>
+                                        </DropdownMenuTrigger>
+
+                                        <DropdownMenuContent >
+                                            {MODELS.options.map((model) => (
+                                                <DropdownMenuItem
+                                                    key={model.label}
+                                                    className={cn('flex text-sm gap-1 items-center p-1.5 cursor-default hover:bg-zinc-100', {
+                                                        "bg-zinc-100": model.label === options.model.label
+                                                    })}
+                                                    onClick={() => {
+                                                        setOptions((prev) => ({ ...prev, model }))
+                                                    }}
+                                                >
+                                                    <Check className={cn('mr-2 h-4 w-4 ',
+                                                        model.label === options.model.label ? "opacity-100" : "opacity-0"
+                                                    )} />
+                                                    {model.label}
+                                                </DropdownMenuItem>
+                                            ))}
+                                        </DropdownMenuContent>
+
+                                    </DropdownMenu>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                 </ScrollArea>
+
             </div>
 
         </div >
--- validators/option-validator.tsx
+++ validators/option-validator.tsx
@@ -18,3 +18,37 @@ export const COLORS = [
         tw: "rose-950"
     },
 ] as const // as const => this will tell type script that this it a const and that's why it will show the exact values that are in it. check by removing the "as const" and hover over CONST then rewrite and check again.  
+
+export const MODELS = {
+    name: "models",
+    options: [
+        {
+            label: "iPhone X",
+            value: "iphonex"
+        },
+        {
+            label: "iPhone 11",
+            value: "iphone11"
+        },
+        {
+            label: "iPhone 12",
+            value: "iphone12"
+        },
+        {
+            label: "iPhone 13",
+            value: "iphone13"
+        },
+        {
+            label: "iPhone 14",
+            value: "iphone14"
+        },
+        {
+            label: "iPhone 15",
+            value: "iphone15"
+        },
+        {
+            label: "iPhone 16",
+            value: "iphone16"
+        },
+    ]
+} as const
